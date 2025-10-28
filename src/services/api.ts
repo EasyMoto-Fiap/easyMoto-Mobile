41d2409fa363ad644abae1d4ead02d2c28cf0a7c
valor-null <valeriaconcedsantos@gmail.com>
2025-10-28 17:56:23 -0300

    feat: padroniza cliente Axios com x-api-key e x-api-version

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,18 @@
 import axios from 'axios';
-import { Platform } from 'react-native';
 
-const PORT = 5230;
+const api = axios.create({
+  baseURL: 'http://10.0.2.2:5230/api',
+  timeout: 15000,
+});
 
-const baseURL =
-  Platform.OS === 'android'
-    ? `http://10.0.2.2:${PORT}/api`   // Android Emulator (Expo)
-    : `http://localhost:${PORT}/api`; // iOS Simulator / Web em dev
+const API_KEY = 'EM_3f8b2a4c9b7e4e6e9a7c1f2d3a5b8c';
+const API_VERSION = '2.0';
 
-export default axios.create({ baseURL, timeout: 15000 });
+api.interceptors.request.use((config) => {
+  config.headers = config.headers ?? {};
+  config.headers['x-api-key'] = API_KEY;
+  config.headers['x-api-version'] = API_VERSION;
+  return config;
+});
+
+export default api;
